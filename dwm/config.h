6fd8d5ed0d2313bceb1c9e3664d03ea134b348f7
zer0reaction <boltgopnik@gmail.com>
2025-02-16 23:22:29 +0300

    Updated font

--- dwm/config.h
+++ dwm/config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "IosevkaNerdFont Mono:size=10" };
-static const char dmenufont[]       = "IosevkaNerdFont Mono:size=10";
+static const char *fonts[]          = { "DejaVu Sans Mono:size=10" };
+static const char dmenufont[]       = "DejaVu Sans Mono:size=10";
 static const char col_gray1[]       = "#002b36";
 static const char col_gray2[]       = "#073642";
 static const char col_gray3[]       = "#839496";
